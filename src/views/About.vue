4e779bf8509c7b4ce2c45bbc1a3d62e1ae124fef
Eduardo Inerate <eduardo.inerate@gig.com>
2019-10-24 17:24:22 +0200

    almos done

--- src/views/About.vue
+++ src/views/About.vue
@@ -41,14 +41,55 @@
                 </v-card-actions>
             </v-card>
         </v-flex>
-        <v-flex class="pa-4 grey lighten-3" md3 xs12>
-            <h2>Related Info </h2>
-            <div class="pa-5" v-for="country in getCountry" :key="country.name">
-                <img :src="country.flag" class="radius7" height="auto" width="150px"/>
-                <h3>Name : {{country.name}}</h3>
+        <v-flex class="grey lighten-3" md3 xs12>
+            <div :key="country.name" class="pa-4" v-for="country in getCountry">
+                <v-card class="info_card">
+                    <v-card-text>
+                        <h2>General Information </h2><br>
+                        <img :src="country.flag" class="radius7" height="auto" width="100%"/>
+                        <p><b>Name :</b> {{country.name}}</p>
+                        <p><b>Country Code :</b> {{country.alpha3Code}}</p>
+                        <p><b>Capital :</b> {{country.capital}}</p>
+                        <p><b>Region :</b> {{country.region}}</p>
+                        <p><b>Population :</b> {{country.population}}</p>
+                        <p><b>Numeric Code :</b> {{country.numericCode}}</p>
+                        <h2>Other Information </h2><br>
+                        <p><b>Borders :</b>
+                        <ul v-if="(country.borders).length > 0">
+                            <li :key="i" class="ml-4" v-for="(borders,i) in country.borders">
+                                {{borders}}
+                            </li>
+                        </ul>
+                        <ul v-else> No Borders</ul>
+                        </p>
+                        <p><b>Currencies :</b>
+                        <ul>
+                            <li :key="i" class="ml-4" v-for="(currencies,i) in country.currencies">
+                                {{currencies.name}} ({{currencies.symbol}}/{{currencies.code}})
+                            </li>
+                        </ul>
+                        </p>
+                        <p><b>Lenguages :</b>
+                        <ul>
+                            <li :key="i" class="ml-4" v-for="(languages,i) in country.languages">
+                                {{languages.name}}
+                            </li>
+                        </ul>
+                        </p>
+                        <p><b>Phone Codes :</b>
+                        <ul>
+                            <li :key="i" class="ml-4" v-for="(callingCodes,i) in country.callingCodes">
+                                {{callingCodes}}
+                            </li>
+                        </ul>
+                        </p>
+                        <v-btn class="primary" dark fab> <v-icon>map</v-icon></v-btn>
+                    </v-card-text>
+                </v-card>
             </div>
         </v-flex>
         <v-flex class="pa-4 grey lighten-4 radius_right" md6 xs12>
+
         </v-flex>
     </v-layout>
 </template>
@@ -77,7 +118,7 @@
             init() {
                 let parameters = {
                     type: 'name',
-                    value: 'spain'
+                    value: 'China'
                 };
                 this.$store.dispatch('fetchCountry', parameters)
             },
